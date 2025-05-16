02bd8f64139a8fd34aaa869941d83aafe807659f
tiago <tiagoribeiro15111@gmail.com>
2025-05-16 15:15:43 +0100

    implement generic taskFlow response, add pom dependencies
    Pom dependencies:
    -> core
    -> lombok
    
    RestConverter: fix bug , because have conflict with RepositoryConverter

---
+++ TaskFlow/infrastructure/src/main/java/com/event/TaskFlow/api/TaskFlowResponse.java
@@ -0,0 +1,58 @@
+package com.event.TaskFlow.api;
+
+import java.io.Serializable;
+
+public class TaskFlowResponse<T> implements Serializable {
+
+    private String status;
+    private String code;
+    private String message;
+    private T data;
+
+    public TaskFlowResponse() {}
+
+    public TaskFlowResponse(String status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    public TaskFlowResponse(String status, T data, String message, String code) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
